c3b7b1ce8419920160df508e48992829d22d430f
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-08-17 15:41:10 +1200

    [feature] Place quicker tests firsts in case crashes.

--- runall.bat
+++ runall.bat
@@ -1,6 +1,6 @@
 @echo off
 
-python stage-boss-one-profile.py --no 10 --dir c:\working\droid-test\govdocs > govdocs-analysis.csv
+python stage-boss-one-profile.py --no 10 --dir c:\working\droid-test\wavs > wavs-analysis.csv
 
 timeout 10
 
@@ -8,30 +8,6 @@ python stage-boss-one-profile.py --no 10 --dir c:\working\droid-test\fake_govdoc
 
 timeout 10
 
-python stage-boss-one-profile.py --no 10 --dir c:\working\droid-test\wavs > wavs-analysis.csv
-
-timeout 10
-
-sf -sig "C:\working\git\digital-preservation-stage-boss-one\siegfried.sigs\container\NOLIMIT-1.6.2-v86-july2016-default.sig" c:\working\droid-test\govdocs > sf-container-NOLIMIT.sf
-
-timeout 10
-
-sf -sig "C:\working\git\digital-preservation-stage-boss-one\siegfried.sigs\no-container\NOLIMIT-1.6.2-v86-july2016-nocontainer-default.sig" c:\working\droid-test\govdocs > sf-no-container-NOLIMIT.sf
-
-timeout 10
-
-sf -sig "C:\working\git\digital-preservation-stage-boss-one\siegfried.sigs\container\10MB-1.6.2-v86-july2016-default.sig" c:\working\droid-test\govdocs > sf-container-10MB.sf
-
-timeout 10
-
-sf -sig "C:\working\git\digital-preservation-stage-boss-one\siegfried.sigs\no-container\10MB-1.6.2-v86-july2016-nocontainer-default.sig" c:\working\droid-test\govdocs > sf-no-container-10MB.sf
-
-timeout 10
-
-sf -sig "C:\working\git\digital-preservation-stage-boss-one\siegfried.sigs\container\65535-1.6.2-v86-july2016-default.sig" c:\working\droid-test\govdocs > sf-container-65535.sf
-
-timeout 10
-
-sf -sig "C:\working\git\digital-preservation-stage-boss-one\siegfried.sigs\no-container\65535-1.6.2-v86-july2016-nocontainer-default.sig" c:\working\droid-test\govdocs > sf-no-container-65535.sf
+python stage-boss-one-profile.py --no 10 --dir c:\working\droid-test\govdocs > govdocs-analysis.csv
 
 timeout 10
